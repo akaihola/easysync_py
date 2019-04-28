174015f20c6d3481a122dadda590899eb35da47f
Antti Kaihola <antti19+etherpad@kaihola.fi>
2019-04-28 22:43:39 +0300

    Automatic `to_python()` conversion using the `HJs()` wrapper
    For functions and methods which have been converted to Python, there's
    now no parameter conversion when calling from Python, whereas upon a
    call from JavaScript code, all parameters will be converted using
    `to_python()` by the `HJs()` wrapper.

--- changeset.py
+++ changeset.py
@@ -2,7 +2,7 @@ import re
 import string
 from typing import Dict, Optional, Union
 
-from js2py.base import to_python
+from js2py.base import HJs
 
 from js_module import eval_js_module
 
@@ -16,11 +16,10 @@ def parseNum(s: str) -> int:
     :return: parsed number
 
     """
-    s = to_python(s)
     return int(s, 36)
 
 
-changeset.parseNum = parseNum
+changeset.parseNum = HJs(parseNum)
 
 
 BASE36_ALPHABET = string.digits + string.ascii_lowercase
@@ -33,7 +32,6 @@ def numToString(num: int) -> str:
     :return: number encoded as a base-36 string
 
     """
-    num = to_python(num)
     base36 = ''
     while num:
         num, i = divmod(num, 36)
@@ -41,33 +39,29 @@ def numToString(num: int) -> str:
     return base36 or BASE36_ALPHABET[0]
 
 
-changeset.numToString = numToString
+changeset.numToString = HJs(numToString)
 
 
 class OpIterator:
-    def __init__(self, opsStr: str, optStartIndex: int):
+    def __init__(self, opsStr: str, optStartIndex: int = 0):
         """Create an iterator which decodes string changeset operations
 
         :param opsStr: String encoding of the change operations to be performed
-        :param optStartIndex: from where in the string should the iterator start
+        :param optStartIndex: from where in the string should the iterator
+                              start
         :return: type object iterator
 
         """
-        self.opsStr = to_python(opsStr)
+        self.opsStr = opsStr
         self.regex = re.compile(
             r'((?:\*[0-9a-z]+)*)(?:\|([0-9a-z]+))?([-+=])([0-9a-z]+)'
             r'|\?'
             r'|')
-        self.startIndex = to_python(optStartIndex) or 0
+        self.startIndex = optStartIndex or 0
         self.curIndex = self.startIndex
         self.prevIndex = self.curIndex
         self.regexResult = self.nextRegexMatch()
         self.obj = Op()
-
-    @staticmethod
-    def new_op(opsStr: str, optStartIndex: int):
-        """JavaScript compatibility constructor"""
-        return OpIterator(opsStr, optStartIndex)
 
     def nextRegexMatch(self):
         self.prevIndex = self.curIndex
@@ -79,13 +73,12 @@ class OpIterator:
         return result
 
     def next(self, optObj: Optional['Op'] = None) -> 'Op':
-        optObj = to_python(optObj)
         op = optObj or self.obj
         if self.regexResult.group(0):
             op.attribs = self.regexResult.group(1)
-            op.lines = changeset.parseNum(self.regexResult.group(2) or '0')
+            op.lines = parseNum(self.regexResult.group(2) or '0')
             op.opcode = self.regexResult.group(3)
-            op.chars = changeset.parseNum(self.regexResult.group(4))
+            op.chars = parseNum(self.regexResult.group(4))
             self.regexResult = self.nextRegexMatch()
         else:
             op.clear()
@@ -98,7 +91,7 @@ class OpIterator:
         return self.prevIndex
 
 
-changeset.opIterator = OpIterator.new_op
+changeset.opIterator = HJs(OpIterator)
 
 
 class Op:
@@ -116,15 +109,6 @@ class Op:
         self.chars = chars
         self.lines = lines
         self.attribs = attribs
-
-    @staticmethod
-    def new_op(optOpcode: Optional[str] = None) -> 'Op':
-        """Create a new Op object from JavaScript
-
-        :param optOpcode: the type operation of the Op object
-
-        """
-        return Op(to_python(optOpcode))
 
     def clear(self):
         """Clean an Op object"""
@@ -151,16 +135,16 @@ class Op:
         :param op2: dest Op
 
         """
-        op2['opcode'] = op1['opcode']
-        op2['chars'] = op1['chars']
-        op2['lines'] = op1['lines']
-        op2['attribs'] = op1['attribs']
+        op2.opcode = op1.opcode
+        op2.chars = op1.chars
+        op2.lines = op1.lines
+        op2.attribs = op1.attribs
 
 
-changeset.newOp = Op.new_op
-changeset.clearOp = Op.clear
-changeset.cloneOp = Op.clone
-changeset.copyOp = Op.copy_op
+changeset.newOp = HJs(Op)
+changeset.clearOp = HJs(Op.clear)
+changeset.cloneOp = HJs(Op.clone)
+changeset.copyOp = HJs(Op.copy_op)
 
 
 def unpack(cs: str) -> Dict[str, Union[int, str]]:
@@ -170,7 +154,6 @@ def unpack(cs: str) -> Dict[str, Union[int, str]]:
     :return: a Changeset data structure
 
     """
-    cs = to_python(cs)
     headerRegex = re.compile(r'Z:([0-9a-z]+)([><])([0-9a-z]+)|')
     headerMatch = headerRegex.search(cs)
     if not headerMatch or not headerMatch.group(0):
@@ -189,7 +172,7 @@ def unpack(cs: str) -> Dict[str, Union[int, str]]:
             'charBank': cs[opsEnd + 1:]}
 
 
-changeset.unpack = unpack
+changeset.unpack = HJs(unpack)
 
 
 def pack(oldLen: int, newLen: int, opsStr: str, bank: str) -> str:
@@ -202,12 +185,10 @@ def pack(oldLen: int, newLen: int, opsStr: str, bank: str) -> str:
     :return: a Changeset string
 
     """
-    oldLen, newLen = to_python(oldLen), to_python(newLen)
-    opsStr, bank = to_python(opsStr), to_python(bank)
     lenDiff = newLen - oldLen
     lenDiffStr = (f'>{numToString(lenDiff)}' if lenDiff >= 0
                   else f'<{numToString(-lenDiff)}')
     return f'Z:{numToString(oldLen)}{lenDiffStr}{opsStr}${bank}'
 
 
-changeset.pack = pack
+changeset.pack = HJs(pack)
